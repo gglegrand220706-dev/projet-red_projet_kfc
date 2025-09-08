19e83aa4e0af4326cd6ec1dfc79d795abbb45c23
glegrand <giovanni.legrand@ynov.com>
2025-09-08 19:07:02 +0200

    commit de la fonction personnage

--- personnage/perso.go
+++ personnage/perso.go
@@ -1,2 +1,28 @@
 package personnage
 
+import "fmt"
+
+type Character struct {
+
+Nom string 
+Classe string 
+Niveau int 
+Viemax int 
+Vieactuelle int 
+Inventaire int []  
+}
+
+
+
+func (u *Character) recupinfo {
+
+fmt.Scanln(&u.Nom) 
+fmt.Println(recupinfo)
+
+}
+
+
+
+
+
+
