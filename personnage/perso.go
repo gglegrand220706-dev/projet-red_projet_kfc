dafed279b26a385149d7d31d6c25714e7e6f6ca4
Robin <imredyt85@gmail.com>
2025-09-08 19:59:25 +0200

    Début du système pour récuperer la classe

--- personnage/perso.go
+++ personnage/perso.go
@@ -14,17 +14,31 @@ Inventaire int
 
 func main() {
 	var joueur Character
-	joueur.RecupInfo()
+	joueur.RecupInfoName()
+	var classe Character
+	classe.RecupInfoClass()
 }
 
-func (u Character) Display() {
+func (u Character) DisplayName() {
 	fmt.Printf("votre pseudo est donc: " + u.Nom)
 }
 
 
-func (u *Character) RecupInfo() {
+
+func (u *Character) RecupInfoName() {
 	fmt.Println("choisisir un pseudo : ")
 	fmt.Scanln(&u.Nom) 
-	u.Display()
+	u.DisplayName()
+
+}
+
+func (u Character) DisplayClass() {
+	fmt.Printf("votre classe est donc: " + u.Classe)
+}
+
+func (u *Character) RecupInfoClass() {
+	fmt.Println("choisisir un pseudo : ")
+	fmt.Scanln(&u.Classe) 
+	u.DisplayClass()
 
 }
