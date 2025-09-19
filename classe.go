93e65c9b31072d0045cdd7c71147fa587655a055
issa-le-goat <issa.vebobe@ynov.com>
2025-09-19 11:06:09 +0200

    réparation git by Giovznni

--- classe.go
+++ classe.go
@@ -1,65 +1,60 @@
 package personnage
 
 type Kryptonien struct {
-	Viemax       int
-	Inventaire   int
-	Puissance    float64
-	Agilite      int
-	Intelligence int
-	CapacityDisplay     []string
-	AttaquesSpé			[]Attaques
-	Capacity			[]Capacity
-
+	Viemax          int
+	Inventaire      int
+	Puissance       float64
+	Agilite         int
+	Intelligence    int
+	CapacityDisplay []string
+	AttaquesSpé     []Attaques
+	Capacity        []Capacity
 }
 
-<<<<<<< HEAD
 var Kryptonien1 = Kryptonien{20, 10, 25, 25, 20, []string{"Laser 🔴"}, []Attaques{AttaqueLasser}, []Capacity{}}
-=======
-var Kryptonien1 = Kryptonien{20, 10, 25, []string{"Crypto 🪙", "Magie ✨", "Puissance plus grande 💥"}, 25, 20, []string{"Laser 🔴"}, []Attaques{AttaqueLasser}, []Capacity{}}
->>>>>>> parent of 32c3a8f (Commit rajout de fonction et réparation design)
 
 type Batfamily struct {
-	Viemax       int
-	Inventaire   int
-	Puissance    float64
-	Agilite      int
-	Intelligence int
-	CapacityDisplay     []string
+	Viemax          int
+	Inventaire      int
+	Puissance       float64
+	Agilite         int
+	Intelligence    int
+	CapacityDisplay []string
 }
 
 var BatFamily1 = Batfamily{10, 15, 10, 35, 50, []string{"Argent", "*2EXP 📈"}}
 
 type Hulk struct {
-	Viemax       int
-	Inventaire   int
-	Puissance    float64
-	Agilite      int
-	Intelligence int
-	CapacityDisplay     []string
-	AttaquesSpé []Attaques
+	Viemax          int
+	Inventaire      int
+	Puissance       float64
+	Agilite         int
+	Intelligence    int
+	CapacityDisplay []string
+	AttaquesSpé     []Attaques
 }
 
 var Hulk1 = Hulk{30, 4, 50, 15, 10, []string{"Clape Sonic 🔊"}, []Attaques{ClapSuperSonic}}
 
 type Kantin struct {
-	Viemax       int
-	Inventaire   int
-	Puissance    float64
-	Agilite      int
-	Intelligence int
-	CapacityDisplay     []string
+	Viemax          int
+	Inventaire      int
+	Puissance       float64
+	Agilite         int
+	Intelligence    int
+	CapacityDisplay []string
 }
 
 var Kantin1 = Kantin{120, 60, 100, 70, 100, []string{"Toutes 🧬", "HO 🧠", "Prout Bruisateurr!! 💨"}}
 
 type Wakandais struct {
-	Viemax       		int
-	Inventaire   		int
-	Puissance    		float64
-	Agilite      		int
-	Intelligence 		int
-	CapacityDisplay     []string
-	AttaquesSpé			[]Attaques
+	Viemax          int
+	Inventaire      int
+	Puissance       float64
+	Agilite         int
+	Intelligence    int
+	CapacityDisplay []string
+	AttaquesSpé     []Attaques
 }
 
 var Wakanda1 = Wakandais{15, 13, 20, 30, 40, []string{"Assimilation des coups pour renvoi synthétique 🛡️🤖"}, []Attaques{RenvoieSinetique}}
